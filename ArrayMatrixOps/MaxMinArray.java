7b4e59e26033c235bb55bef4b4921a59ffaa22ec
Ranbeer Kamlapure <ranbeerk036@gmail.com>
2025-07-13 13:16:48 +0530

    Add new files

---
+++ ArrayMatrixOps/MaxMinArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MaxMinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+        System.out.println("Enter 10 integers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+
+        double average = sum / 10.0;
+        System.out.println("Average: " + average);
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
